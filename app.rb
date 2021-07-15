51f89ab3c18fb608638aa55dc5835300d54c4cd1
PeaceS <peace@glazziq.com>
2021-07-16 06:17:47 +0700

    change to hash

--- app.rb
+++ app.rb
@@ -11,43 +11,61 @@ FunctionsFramework.http :update_cartoon_list do |_request|
   response = HTTParty.get('https://www.niceoppai.net')
   raise 'Something wrong with http read' if response.body.nil? || response.body.empty?
 
-  html = Nokogiri::HTML(response.body)
+  html_object = Nokogiri::HTML(response.body)
 
-  object = find_by(object: html, type: 'lang', value: 'en-US')
-  object = find_by_name(object: object, name: 'body')
-  object = find_by(object: object, type: 'class', value: 'wrap')
-  object = find_by(object: object, type: 'id', value: 'sct_col_l')
-  object = find_by(object: object, type: 'id', value: 'sct_wid_bot')
-  object = find_by_name(object: object, name: 'ul')
-  object = find_by_name(object: object, name: 'li')
-  object = find_by(object: object, type: 'class', value: 'con')
-  object = find_by(object: object, type: 'class', value: 'textwidget')
-  object = find_by(object: object, type: 'class', value: 'wpm_pag mng_lts_chp grp')
-  objects = loop_by(object: object, type: 'class', value: 'row')
+  html_object = [
+    html_object,
+    { 'lang' => 'en-US' },
+    { 'body' => nil },
+    { 'class' => 'wrap' },
+    { 'id' => 'sct_col_l' },
+    { 'id' => 'sct_wid_bot' },
+    { 'ul' => nil },
+    { 'li' => nil },
+    { 'class' => 'con' },
+    { 'class' => 'textwidget' },
+    { 'class' => 'wpm_pag mng_lts_chp grp' }
+  ].reduce do |object, node|
+    find_by(object: object, type: node.first[0], value: node.first[1])
+  end
 
-  objects = objects.map do |obj|
-    object = find_by(object: obj, type: 'class', value: 'det')
-    name_object = find_by_name(object: object, name: 'a')
+  html_objects = loop_by(object: html_object, type: 'class', value: 'row')
+
+  cartoon_data = html_objects.map do |html_object|
+    html_object = find_by(object: html_object, type: 'class', value: 'det')
+
+    name_object = find_by(object: html_object, type: 'a')
     name = name_object.children[0].text.strip
-    object = find_by_name(object: object, name: 'ul')
-    object = find_by_name(object: object, name: 'li')
-    object = find_by_name(object: object, name: 'a')
-    [name, name_object.attributes['href'].value, object.attributes['href'].value.split('/').last.to_f, object.attributes['href'].value]
+
+    html_object = [
+      html_object,
+      { 'ul' => nil },
+      { 'li' => nil },
+      { 'a' => nil },
+    ].reduce do |object, node|
+      find_by(object: object, type: node.first[0], value: node.first[1])
+    end
+
+    [
+      name,
+      name_object.attributes['href'].value,
+      html_object.attributes['href'].value.split('/').last.to_f,
+      html_object.attributes['href'].value
+    ]
   end
 
   firestore = Google::Cloud::Firestore.new project_id: PROJECT_ID, credentials: 'keys/niceoppai-notifier-bea93adf3021.json'
 
-  puts "total of #{objects.size} cartoons in the list"
-  created_list = []
+  puts "total of #{cartoon_data.size} cartoons in the list"
 
   latest_chapters = firestore.transaction do |transaction|
-    objects.map do |data|
+    cartoon_data.map do |data|
       doc = firestore.doc format('%<collection>s/%<name>s', collection: CARTOON_LIST_COLLECTION, name: data[0])
       transaction.get(doc).data&.[](:latest_chapter)
     end
   end
 
-  updated_list = objects.zip(latest_chapters).map do |data, latest_chapter|
+  updated_list = cartoon_data.zip(latest_chapters).map do |data, latest_chapter|
     next if latest_chapter && latest_chapter == data[2].to_f
 
     doc = firestore.doc(format('%<collection>s/%<name>s', collection: CARTOON_LIST_COLLECTION, name: data[0]))
@@ -56,38 +74,25 @@ FunctionsFramework.http :update_cartoon_list do |_request|
       latest_chapter: data[2],
       latest_link: data[3]
     }, merge: true)
-
-    unless latest_chapter
-      created_list << data[0]
-      nil
-    else
-      data[0]
-    end
   end.compact
 
-  puts "created new #{created_list.size} cartoons, updated #{updated_list.size} cartoons"
-
   unless updated_list.empty?
+    puts "updated #{updated_list.size} cartoons"
     puts 'updated list:'
     updated_list.each { |cartoon| puts cartoon }
-  end
-
-  unless created_list.empty?
-    puts 'created list:'
-    created_list.each { |cartoon| puts cartoon }
+  else
+    puts "no updated cartoons"
   end
 
   'ok'
 end
 
-def find_by(object:, type:, value:)
-  object.children.find do |data|
-    data.attributes[type]&.value == value
+def find_by(object:, type:, value: nil)
+  if value
+    object.children.find { |data| data.attributes[type]&.value == value }
+  else
+    object.children.find { |data| data.name == type }
   end
-end
-
-def find_by_name(object:, name:)
-  object.children.find { |data| data.name == name }
 end
 
 def loop_by(object:, type:, value:)
